a62fedf2150edb8ef389281dfbda0fa3000b901f
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-05-23 20:46:41 +0200

    Add mapping for lspsaga hover_doc

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Open documentation hover"})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
