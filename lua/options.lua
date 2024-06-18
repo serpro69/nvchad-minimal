0ee7a869bc7a301cb6cdad76cd9fcf4938ea2482
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-06-18 07:39:10 +0200

    Set relativenumber and some fold options

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,20 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+o.relativenumber = true
+
+-- set treesitter fold on expressions (from Andy)
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldtext = ""
+o.foldenable = false
+o.foldnestmax = 2
+o.foldlevel = 99
+o.foldlevelstart = 1
+o.foldcolumn = "0"
+o.cursorline = false
+-- vim.g.markdown_folding = 1
+
