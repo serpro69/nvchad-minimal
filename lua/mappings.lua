2bf805437a196fa569a321ebde2c535be647f5b4
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-06-28 10:26:28 +0200

    Add mapping for closing all buffers

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,8 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- lspsaga
-map("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Open documentation hover" })
-map("n", "<leader>lt", "<cmd>Lspsaga term_toggle<CR>", { desc = "Open floating terminal" })
-map("n", "<leader>lc", "<cmd>Lspsaga code_action<CR>", { desc = "Run code-action" })
+map("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Lspsaga open documentation hover" })
+map("n", "<leader>lt", "<cmd>Lspsaga term_toggle<CR>", { desc = "Lspsaga open floating terminal" })
+map("n", "<leader>lc", "<cmd>Lspsaga code_action<CR>", { desc = "Lspsaga run code-action" })
+
+map("n", "<leader>cx", function()
+  require("nvchad.tabufline").closeAllBufs()
+end, { desc = "Buffer close all" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
