39d00f5ab2f52df88f1517646cdba70fd75378ce
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-07-01 12:33:35 +0200

    Enable format-on-save for conform plugin

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -207,9 +207,9 @@ M.ui = {
       blank = function()
         return "%#Normal#" .. "%=" -- empty space
       end,
-      -- custom_btns = function()
-      --   return " %#Normal#%@v:lua.ClickGit@  %#Normal#%@v:lua.RunCode@  %#Normal#%@v:lua.ClickSplit@  "
-      -- end,
+      custom_btns = function()
+        return " %#Normal#%@v:lua.ClickGit@  %#Normal#%@v:lua.RunCode@  %#Normal#%@v:lua.ClickSplit@  "
+      end,
     },
   },
 
--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,16 +1,16 @@
 local options = {
   formatters_by_ft = {
+    css = { "prettier" },
+    html = { "prettier" },
     lua = { "stylua" },
-    tf = { "tflint", "tfsec" }
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    tf = { "tflint", "tfsec" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
