a02317d17ed2b1cea266a4995f49bfdf402813a0
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-08-16 13:40:51 +0200

    Update nvdash header and add actions-preview.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -83,6 +83,29 @@ return {
   },
 
   {
+    "aznhe21/actions-preview.nvim",
+    config = function()
+      vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+      require("actions-preview").setup {
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        },
+      }
+    end,
+    event = "LspAttach",
+  },
+
+  {
     "cuducos/yaml.nvim",
     ft = { "yaml", "yaml.ansible" }, -- optional
     dependencies = {
