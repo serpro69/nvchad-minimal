c3f205c6cb87628181299585a00836c430a4dcd2
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-08-17 13:12:16 +0200

    Overwrite lspsaga code-action mapping with actions-preview

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,12 +10,14 @@ map("i", "jk", "<ESC>")
 -- lspsaga
 map("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", { desc = "Lspsaga open documentation hover" })
 map("n", "<leader>lt", "<cmd>Lspsaga term_toggle<CR>", { desc = "Lspsaga open floating terminal" })
-map("n", "<leader>lc", "<cmd>Lspsaga code_action<CR>", { desc = "Lspsaga run code-action" })
+-- map("n", "<leader>lc", "<cmd>Lspsaga code_action<CR>", { desc = "Lspsaga run code-action" })
 
 map("n", "<leader>xx", "<cmd>buffer close<CR>", { desc = "Buffer close" })
 map("n", "<leader>xa", function()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Buffer close all" })
+
+map({ "v", "n" }, "<leader>lc", require("actions-preview").code_actions, { desc = "LSP Code action" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -90,7 +90,6 @@ return {
   {
     "aznhe21/actions-preview.nvim",
     config = function()
-      vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
       require("actions-preview").setup {
         telescope = {
           sorting_strategy = "ascending",
